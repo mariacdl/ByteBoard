c7d1378fdc4ea4171aade66148eb9b79ad7460a4
Alejandro <alejandro.beltran.suarez@alumnos.upm.es>
2025-03-26 16:09:07 +0100

    añadiendo librerias
    añadiendo librerias para confirmar que está bien sincronizado el github

--- Byte_Boards/main.cpp
+++ Byte_Boards/main.cpp
@@ -1,9 +1,13 @@
 
 
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    cout << "Hello World!" << endl;
     return 0;
 }
